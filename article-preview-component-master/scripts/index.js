5448c7fd26dd2499c28c9b66edc3cc61a2ccfff0
shun yuan <tanshunyuan99@gmail.com>
2021-06-12 14:43:52 +0800

    feat:update shit

---
+++ article-preview-component-master/scripts/index.js
@@ -0,0 +1,5 @@
+const shareBtn = document.querySelector(".share");
+const shareContainer = document.querySelector(".share-container");
+shareBtn.addEventListener("click", () => {
+  shareContainer.classList.toggle("block");
+});
